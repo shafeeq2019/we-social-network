c50e8b5e2f72bcfb063a404ee437c8540a8f3d61
shafeeq2019 <barram.shafeeq@gmail.com>
2023-12-24 15:00:01 +0100

    add static signup page

---
+++ soical-network-frontend/src/views/SignupView.vue
@@ -0,0 +1,56 @@
+<template lang="">
+        <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
+                <div class="main-left col-span-1">
+                    <div class="p-12 bg-white border border-gray-200 rounded-lg">
+                        <h1 class="mb-6 text-2xl">Log in</h1>
+
+                        <p class="mb-6 text-gray-500">
+                            Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate.
+                            Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate.
+                        </p>
+
+                        <p class="font-bold">
+                            Already have an account? Click here to log in!
+                        </p>
+                    </div>
+                </div>
+
+                <div class="main-right col-span-1">
+                    <div class="p-12 bg-white border border-gray-200 rounded-lg">
+                        <form class="space-y-6">
+                            <div>
+                                <label>Name</label><br>
+                                <input type="text" placeholder="Your e-mail address" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+                            </div>
+
+                            <div>
+                                <label>E-mail</label><br>
+                                <input type="email" placeholder="Your e-mail address" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+                            </div>
+
+                            <div>
+                                <label>Password</label><br>
+                                <input type="password" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+                            </div>
+
+                            <div>
+                                <label>Repeate password</label><br>
+                                <input type="password" placeholder="Repeate your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+                            </div>
+
+                            <div>
+                                <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Signup</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
