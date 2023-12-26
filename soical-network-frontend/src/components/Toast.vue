fb4cf12e83371def33bf205c2a5bba41790f7755
shafeeq2019 <barram.shafeeq@gmail.com>
2023-12-26 23:14:17 +0100

    Toast Bug fixed

--- soical-network-frontend/src/components/Toast.vue
+++ soical-network-frontend/src/components/Toast.vue
@@ -1,20 +1,13 @@
 <template>
-    <div
-        v-if="store.isVisible"
+    <div v-if="store.isVisible"
         class="transition ease-in-out delay-500 duration-500 px-6 py-6 fixed top-full right-8 rounded-xl shadow-xl"
-        :class="store.classes"
-    >
+        :class="store.classes">
         {{ store.message }}
     </div>
 </template>
 
-<script>
+<script setup>
 import { useToastStore } from '@/stores/toast'
+
 const store = useToastStore()
-export default {
-    
-}
 </script>
-<style lang="">
-    
-</style>
