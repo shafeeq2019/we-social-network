63c8ab43c26c3836fc0d181c2e5189888c515bec
shafeeq2019 <barram.shafeeq@gmail.com>
2023-12-26 22:35:51 +0100

    Change Trends and People you may know to components

---
+++ soical-network-frontend/src/components/Trends.vue
@@ -0,0 +1,39 @@
+<template lang="">
+    <div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-xl">Trends</h3>
+
+        <div class="space-y-4">
+
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>#Gaming</strong><br>
+                    <span class="text-gray-500">180 posts</span>
+                </p>
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>#Newyear</strong><br>
+                    <span class="text-gray-500">180 posts</span>
+                </p>
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>#Coding</strong><br>
+                    <span class="text-gray-500">180 posts</span>
+                </p>
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
+<style lang="">
+
+</style>
--- soical-network-frontend/src/views/FeedView.vue
+++ soical-network-frontend/src/views/FeedView.vue
@@ -138,78 +138,20 @@
 
         <!-- People you may know -->
         <div class="main-right col-span-1 space-y-4">
-            <div class="p-4 bg-white border border-gray-200 rounded-lg">
-                <h3 class="mb-6 text-xl">People you may know</h3>
-
-                <div class="space-y-4">
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-
-                            <p class="text-xs"><strong>Max</strong></p>
-                        </div>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-
-                            <p class="text-xs"><strong>Johny</strong></p>
-                        </div>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-2">
-                            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-
-                            <p class="text-xs"><strong>Jens</strong></p>
-                        </div>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="p-4 bg-white border border-gray-200 rounded-lg">
-                <h3 class="mb-6 text-xl">Trends</h3>
-
-                <div class="space-y-4">
-                
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#Gaming</strong><br>
-                            <span class="text-gray-500">180 posts</span>
-                        </p>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#Newyear</strong><br>
-                            <span class="text-gray-500">180 posts</span>
-                        </p>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#Coding</strong><br>
-                            <span class="text-gray-500">180 posts</span>
-                        </p>
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                </div>
-            </div>
+            <PeopleYouMayKnow />
+            <Trends />
         </div>
 
     </div>
 </template>
 <script>
-    export default {};
+    import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
+    import Trends from '../components/Trends.vue'
+    export default {
+        components: {
+            PeopleYouMayKnow,
+            Trends
+        }
+    };
 </script>
 <style lang=""></style>
