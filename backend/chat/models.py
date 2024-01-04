e1ab43ce73a0985a6968e78ba5408429df485302
shafeeq2019 <barram.shafeeq@gmail.com>
2024-01-04 18:35:10 +0100

    added chat models

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid
+from account.models import User
 
-# Create your models here.
+
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    users = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class ConversationMessage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    conversation = models.ForeignKey(
+        Conversation, related_name=("messages"), on_delete=models.CASCADE)
+    message = models.TextField()
+    sent_to = models.ForeignKey(User,  related_name=(
+        "received_messages"), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User,  related_name=(
+        "sent_messages"), on_delete=models.CASCADE)
