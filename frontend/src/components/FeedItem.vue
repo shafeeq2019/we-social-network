5a7dab83ce96fb2ea11e65e8c50d389e9978eeec
shafeeq2019 <barram.shafeeq@gmail.com>
2024-02-08 13:34:17 +0100

    added icons to FeedItem dropdown-menu

--- frontend/src/components/FeedItem.vue
+++ frontend/src/components/FeedItem.vue
@@ -58,20 +58,34 @@
 
             <div>
                 <DropdownMenu>
-                    <DropdownMenuTrigger> 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <DropdownMenuTrigger>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z">
                             </path>
                         </svg>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent>
-                        <DropdownMenuItem class="cursor-pointer" @click="deletePost"
-                            v-if="userStore.user.id === post.created_by.id">Delete post</DropdownMenuItem>
+                        <DropdownMenuItem class="cursor-pointer flex space-x-2 items-center" @click="deletePost"
+                            v-if="userStore.user.id === post.created_by.id">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                            <p>Delete post</p>
+                        </DropdownMenuItem>
                         <!-- <DropdownMenuSeparator /> -->
-                        <DropdownMenuItem v-if="userStore.user.id != post.created_by.id" class="cursor-pointer"
-                            @click="openReportModal = true">Report</DropdownMenuItem>
+                        <DropdownMenuItem v-if="userStore.user.id != post.created_by.id"
+                            class="cursor-pointer flex space-x-2 items-center" @click="openReportModal = true">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                            </svg>
+                            <p>Report</p>
+                        </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
