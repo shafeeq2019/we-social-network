1eb35b0f4146fdbcbcbc67fb979d4cd07d920ab7
shafeeq2019 <barram.shafeeq@gmail.com>
2024-02-08 13:21:09 +0100

    added show profile button in conversation options

--- frontend/src/views/MessageView.vue
+++ frontend/src/views/MessageView.vue
@@ -1,8 +1,6 @@
 /**
 TODO:
 - scroll to the end of the chat automatically when open it
-- delete message Button
-- dont show empty conversation
 - real time messaging
 */
 
@@ -22,7 +20,8 @@ TODO:
                     </div>
                     <div>
                         <DropdownMenu>
-                            <DropdownMenuTrigger class="sm:invisible group-hover:visible hover:bg-gray-400 hover:rounded-xl">
+                            <DropdownMenuTrigger
+                                class="sm:invisible group-hover:visible hover:bg-gray-400 hover:rounded-lg mx-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24" stroke-width="1.5"
                                     stroke="currentColor" class="w-8 h-8 block">
                                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -31,12 +30,26 @@ TODO:
                                 </svg>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent>
-                                <DropdownMenuItem class="cursor-pointer" @click="deleteConversation">Delete Conversation
+                                <DropdownMenuItem class="cursor-pointer flex space-x-2 items-center" @click="$router.push({ name: 'profile', params: { id: conversation.users[0].id } })">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>
+                                    <p>Show profile</p>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem class="cursor-pointer flex space-x-2 items-center"
+                                    @click="deleteConversation">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+                                    <p>Delete Conversation</p>
                                 </DropdownMenuItem>
                             </DropdownMenuContent>
                         </DropdownMenu>
                     </div>
-
                 </div>
             </div>
         </div>
