dd3e3de4089e5aa0f63632905c15bce0f14534d0
shafeeq2019 <barram.shafeeq@gmail.com>
2024-02-11 17:11:50 +0100

    fixed bug of sending a message twice

--- frontend/src/views/MessageView.vue
+++ frontend/src/views/MessageView.vue
@@ -92,14 +92,14 @@ TODO:
                 <form @submit.prevent="submitForm">
                     <div class="p-4 flex space-x-3">
                     <input type="text" name="" id="" class="w-full bg-gray-100 rounded-lg p-4 " v-model="messageText"
-                            @keydown.enter.prevent="submitForm" placeholder="What do you want to say?"/>
-                            <button href="#" class="inline-block py-3 px-3 bg-purple-600 text-white rounded-lg" @click="submitForm">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                            </svg>
-                        </button>
+                        @keydown.enter.prevent="submitForm" placeholder="What do you want to say?">
+                    <button class="inline-block py-3 px-3 bg-purple-600 text-white rounded-lg" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                        </svg>
+                    </button>
                     </div>
                 </form>
             </div>
@@ -195,6 +195,7 @@ export default defineComponent({
             }
         },
         async openConversation(conversation: Conversation) {
+            this.messageText = '';
             await this.$router.push({
                 name: 'messages',
                 params: {
