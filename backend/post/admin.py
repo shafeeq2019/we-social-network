d92f0b09f74e4ad3c6090195f595681e309102f7
shafeeq2019 <barram.shafeeq@gmail.com>
2024-02-14 12:15:48 +0100

    added models to admin

--- backend/post/admin.py
+++ backend/post/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Post, PostAttachment
+from .models import Post, PostAttachment, Like, Comment, Reports, Trend
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(PostAttachment)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Reports)
+admin.site.register(Trend)
