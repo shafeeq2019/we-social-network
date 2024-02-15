321a780d0616573780b3e4d0cd983f35aa925f96
shafeeq2019 <barram.shafeeq@gmail.com>
2024-02-15 21:55:07 +0100

    show "there is no trends" if there is no trends

--- frontend/src/components/Trends.vue
+++ frontend/src/components/Trends.vue
@@ -4,9 +4,9 @@
 */ 
 
 <template>
-    <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-md" v-if="trends && trends.length > 0">
+    <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-md">
         <h3 class="mb-6 text-xl">Trends</h3>
-        <div class="space-y-4">
+        <div class="space-y-4" v-if="trends && trends.length > 0">
             <div class="flex items-center justify-between" v-for="trend in trends">
                 <p class="text-xs">
                     <strong>#{{ trend.hashtag }}</strong><br>
@@ -15,6 +15,14 @@
                 <router-link :to="{ name: 'trendsview', params: { 'hashtag': trend.hashtag } }"
                     class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</router-link>
             </div>
+        </div>
+        <div v-else class="flex items-center justify-center">
+            <p class="pr-2 md:text-sm text-gray-700 font-medium">There are no new trends in the last 24 hours</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="w-9 h-9 text-gray-700">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
+            </svg>
         </div>
     </div>
 </template>
